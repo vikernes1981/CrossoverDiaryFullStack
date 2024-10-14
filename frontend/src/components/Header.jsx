46a4073374ca75657f3fb441abe799452c2835ee
vikernes1981 <kostaspapas321@gmail.com>
2024-10-14 15:47:18 +0200

    Small fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,11 +4,10 @@ import PropTypes from 'prop-types';
 
 function Header({ onAddEntryClick }) {
   return (
-    <header className="flex-wrap justify-center items-center mb-6">
+    <header className="flex flex-col items-center mb-6">
       <h1 className="text-6xl font-extrabold text-yellow-700">My Diary</h1>
-      <br />
       <button
-        className="bg-yellow-900 text-yellow-300 px-5 py-2 rounded-lg hover:bg-yellow-200 transition-colors"
+        className="mt-4 bg-yellow-900 text-yellow-300 px-5 py-2 rounded-lg hover:bg-yellow-200 transition-colors"
         onClick={onAddEntryClick}
       >
         Add Entry
